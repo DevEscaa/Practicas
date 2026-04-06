6b88c5876dd22816d34da8577b00ebdd7c2c935f
Hector_M <escaa2714@gmail.com>
2026-04-06 15:24:43 -0500

    Reorganizacion de archivos

--- Visual Studio Code/Practicas/Ejercicio 2/CalcularMedia.js
+++
@@ -1,9 +0,0 @@
-
-media = 0
-
-for(let i = 0; i < 3; i++) {
-    numero = parseInt(prompt("Ingrese el numero para calcular la media. "))
-    media += numero
-}
-resultado = media / 3
-console.log(resultado)
--- Visual Studio Code/Practicas/Ejercicio 2/calcularMedia.py
+++
@@ -1,6 +0,0 @@
-media = 0
-for i in range (3):
-    numero = int(input("ingrese el numero para calcular la media: "))
-    media += numero 
-resultado = media / 3
-print(resultado)
